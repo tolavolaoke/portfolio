03e7b9d7d0b160f019e15aacc1785ee001f154b5
Tola Olaoke <tolaoke@spartaglobal.co>
2017-05-17 12:06:55 +0100

    update css, content

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ My Sparta online portfolio and cv
 
 #Bulit with
 Bootstap 4 framework
-HTML, CSS, SASS
+HTML, CSS
 
 #Author
 T olaoke
--- main2.js
+++
@@ -1,5 +0,0 @@
-$('.hamb-wrap').on('click', function(){
-  $(this).parent().children('p').toggle();
-  $(this).children().toggleClass('active');
-  $('nav').fadeToggle(200);
-}); 
